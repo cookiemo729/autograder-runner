ff15570fcefd2f6db9869e5282654aed89236e68
cookiemo729 <58022041+cookiemo729@users.noreply.github.com>
2026-08-12 09:21:04 +0800

    Replaced with sample solutions

--- submissions/cs201lab1q1/student1/NumbersArray.java
+++ submissions/cs201lab1q1/student1/NumbersArray.java
@@ -20,9 +20,13 @@ public class NumbersArray {
     }
     
     public static int findMax(Integer[] input){
-        while (true) {
-        }
-
+        int max = input[0];
+        for (int i = 1; i < input.length; i++) {
+            if (input[i] > max){
+                max = input[i];
+            }        
+        } 
+        return max;
     }
 
     public static int findMax_A(Integer[] input){
